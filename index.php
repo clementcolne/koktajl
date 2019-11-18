df85c651281bd3fe3172ada981979c122d5b99ce
Clément Colné <43238252+colne5u@users.noreply.github.com>
2019-11-18 21:07:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+// page principale du site au chargement
+// plus tard on utilisera le template 
+// pour l'instant on utilise cette page pour les test d'affichage de nos données cocktails, user et recherches
