6366f397d0ea2c77a77b6ef0063772e75e2ecd12
Clément Colné <43238252+colne5u@users.noreply.github.com>
2019-11-19 21:32:40 +0100

    Update install.php

--- database/install.php
+++ database/install.php
@@ -1,1 +1,46 @@
-// fichier dans lequel écrire la création de la base de données et les requêtes en PHP
+<?php
+
+  include './databaseQueries.php';
+
+  /**
+   * Effectue la conneixon à la DB
+   * et crée les différentes tables de celle-ci
+   * @return mysqli objet correspondant à la DB
+   */
+  function connectDatabase() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $databaseName = "myDB";
+
+    // Connexion à MySql
+    $db = new mysqli($servername, $username, $password);
+    // Vérifie si la connexion est ok
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+    foreach(explode(';', createDatabase($databaseName)) as $sql) {
+      echo "</br>$sql </br>";
+      $bool = $db->query($sql);
+      if ($bool) {
+          echo "Success";
+      } else {
+          echo "Error creating database: " . $db->error;
+      }
+    }
+
+    return $db;
+  }
+
+  /**
+   * Ferme la connexion à la DB
+   * @param  mysqli $db objet correspondant à la DB
+   */
+  function quitDatabase($db) {
+    $db->close();
+  }
+
+  $conn = connectDatabase();
+
+
+?>
